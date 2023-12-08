5d02d96b545fa3c52ced7fbe3eacea22d29c2750
Mounir <lm_melzi@esi.dz>
2023-12-08 21:34:11 +0100

    create .env file

--- backend/settings.py
+++ backend/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv.load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
