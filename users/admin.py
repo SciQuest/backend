146f6abbf5e008f19dd9ebf869813fef4a2b37a1
Mounir <lm_melzi@esi.dz>
2023-12-09 11:52:28 +0100

    register the User model in the admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
-class Admin(UserAdmin):
-    model = models.User
-    list_display = ["email", "first_name", "last_name", "role"]
-    ordering = ["email"]
-
-
-admin.site.register(models.User, Admin)
+admin.site.register(models.User)
