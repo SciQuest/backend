09ad816c8ee016747c005207b777fa2060f46dca
Mounir <lm_melzi@esi.dz>
2023-12-09 04:18:32 +0100

    use Django Admin with the custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+class Admin(UserAdmin):
+    model = models.User
+    list_display = ["email", "first_name", "last_name", "role"]
+    ordering = ["email"]
+
+
+admin.site.register(models.User, Admin)
