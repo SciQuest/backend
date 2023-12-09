18bb8930c121439ba3dfe97b9bb3823cd621bd27
Mounir <lm_melzi@esi.dz>
2023-12-09 03:30:41 +0100

    change auth endpoint name

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    path("auth/", include("users.urls")),
     path("api/", include("api.urls")),
 ]
