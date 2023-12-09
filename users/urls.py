4713608b9f15bc592b8c86bde6e1d97be5f177c8
Mounir <lm_melzi@esi.dz>
2023-12-09 19:49:39 +0100

    create user registration endpoint

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path(
-        "token/",
-        views.CustomTokenObtainPairView.as_view(),
-        name="token_obtain_pair",
-    ),
+    path("register/", views.RegisterUserView.as_view()),
+    path("token/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from . import serializers
+from . import models
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -11,3 +19,22 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             return token
 
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class RegisterUserView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request: Request):
+        if (
+            not isinstance(request.user, AnonymousUser)
+            and request.user.role == models.Role.ADMIN
+        ):
+            request.data["role"] = models.Role.MODERATOR
+        else:
+            request.data["role"] = models.Role.USER
+
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
