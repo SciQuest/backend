3eff9e0f267c2ba267a900912f4dd64863ec5206
Mounir <lm_melzi@esi.dz>
2023-12-11 19:33:28 +0100

    hash the user's password before saving it in the database

--- users/serializers.py
+++ users/serializers.py
@@ -17,3 +17,11 @@ class UserSerializer(ModelSerializer):
         )
         read_only_fields = ("id", "date_joined", "last_login")
         extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
