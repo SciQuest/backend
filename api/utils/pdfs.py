7110a25c231211053af68a8d3043a8b17bfaab39
Mounir <lm_melzi@esi.dz>
2023-12-27 14:41:00 +0100

    extract pdf's ascii text to python string

--- api/utils/pdfs.py
+++ api/utils/pdfs.py
@@ -1,4 +1,12 @@
+import fitz
+
+
 def extract_data(pdf_path: str) -> dict[str, str | list[str]]:
+    pdf_text = ""
+    with fitz.open(pdf_path) as pdf_file:
+        pdf_text = "[PAGE DELIMITER]\n".join([page.get_text() for page in pdf_file])
+        pdf_text = "".join(char for char in pdf_text if ord(char) < 128)
+
     data = {
         "title": "",
         "abstract": "",
