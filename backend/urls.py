626252fe7bb6e635e96bae068ef60ee7e9731e16
Mounir <lm_melzi@esi.dz>
2023-12-27 12:50:30 +0100

    make the media files (pdfs) public

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +25,6 @@ urlpatterns = [
     path("auth/", include("users.urls")),
     path("api/", include("api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
