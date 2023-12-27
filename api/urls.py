65b5cf68029d3a6f4b927bec50b55334bc792574
Mounir <lm_melzi@esi.dz>
2023-12-27 01:38:49 +0100

    upload a pdf file to the server and create a model and a document for it

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("upload/", views.ArticleUploadView.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from users.utils.decorators import protected
+from users.models import Role
+from .utils.pdfs import extract_data
+from . import documents
+from . import serializers
 
-# Create your views here.
+
+class ArticleUploadView(APIView):
+    def post(self, request: Request):
+        return self._post(request)
+
+    @staticmethod
+    @protected(allowed_roles=[Role.ADMIN])
+    def _post(request: Request):
+        serializer = serializers.ArticleSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        article_document = documents.ArticleDocument.get(serializer.data["id"])
+        document_data = extract_data(serializer.data["pdf"][1:])
+
+        for key, value in document_data.items():
+            setattr(article_document, key, value)
+        article_document.save()
+
+        return Response(article_document.to_dict(), status=status.HTTP_201_CREATED)
