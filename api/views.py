e8500f535449c23a0ea1ba52c55a2c597a09a9bd
Mounir <lm_melzi@esi.dz>
2023-12-30 12:33:27 +0100

    create document's data automatically when the model is created or updated

--- api/views.py
+++ api/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.utils.decorators import protected
 from users.models import Role
-from .utils.pdfs import extract_data
 from . import documents
 from . import serializers
 
@@ -21,11 +20,8 @@ class ArticleUploadView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
-        article_document = documents.ArticleDocument.get(serializer.data["id"])
-        document_data = extract_data(serializer.data["pdf"][1:])
 
-        for key, value in document_data.items():
-            setattr(article_document, key, value)
-        article_document.save()
-
-        return Response(article_document.to_dict(), status=status.HTTP_201_CREATED)
+        return Response(
+            documents.ArticleDocument.get(serializer.data["id"]).to_dict(),
+            status=status.HTTP_201_CREATED,
+        )
