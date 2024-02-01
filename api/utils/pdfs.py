cc0fe02e2ac70d0e84e54ae09c5c3d55a7f6d196
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2024-02-01 17:35:27 +0100

    fixed references extraction using regular expressions

--- api/utils/pdfs.py
+++ api/utils/pdfs.py
@@ -1,4 +1,5 @@
 from datetime import date
+from PyPDF2 import PdfReader
 import re
 import os
 from openai import OpenAI
@@ -6,13 +7,40 @@ from dotenv import load_dotenv
 import json
 
 
+def extract_references(text):
+        pattern = re.compile(r"(?is).*references(.*)")#legendary regular expression LOL it solves 90% of our problems
+        match = pattern.search(text)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
 def extract_data(pdf_path: str) -> dict[str, str | list[str]]:
     pdf_text = ""
-    with fitz.open(pdf_path) as pdf_file:
-        pdf_text = "[PAGE DELIMITER]\n".join([page.get_text() for page in pdf_file])
+    
+    reader = PdfReader(pdf_file)
+    pdf_text = ""
+    with open(pdf_file, "rb") as pdf_file:
+        reader=PdfReader(pdf_file)
+        pdf_text = "\n".join([reader.pages[i].extract_text() for i in range(len(reader.pages))])
         pdf_text = "".join(char for char in pdf_text if ord(char) < 128)
+        #print(pdf_text)
+        
+    num_pages = len(reader.pages)
+    first_pages = [reader.pages[i] for i in range(num_pages) if (i < 1)] # first page
 
-     
+    text1 = ""
+    text2 = ""
+
+    for page in first_pages:
+        text1 +=  page.extract_text()
+        text1+= "\n"
+
+    # Use regular expression to extract everything after "References" 
+    references = extract_references(pdf_text)
+    print(references)    
+
+        
     
     data = {
         "title": pdf_path,
