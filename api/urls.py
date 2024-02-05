6c4b264e07a0137ac8e29f6502ce642551a13d15
Mounir <lm_melzi@esi.dz>
2024-02-05 09:20:28 +0100

    name the app urls

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,17 @@ from . import views
 
 
 urlpatterns = [
-    path("articles/", views.ArticlesView.as_view()),
-    path("articles/search/", views.ArticleSearchView.as_view({"get": "list"})),
-    path("articles/<int:article_id>/", views.ArticleView.as_view()),
-    path("favorites/", views.FavoriteArticlesView.as_view()),
-    path("favorites/<int:article_id>/", views.FavoriteArticleView.as_view()),
+    path("articles/", views.ArticlesView.as_view(), name="articles"),
+    path(
+        "articles/search/",
+        views.ArticleSearchView.as_view({"get": "list"}),
+        name="search",
+    ),
+    path("articles/<int:article_id>/", views.ArticleView.as_view(), name="article"),
+    path("favorites/", views.FavoriteArticlesView.as_view(), name="favorites"),
+    path(
+        "favorites/<int:article_id>/",
+        views.FavoriteArticleView.as_view(),
+        name="favorite",
+    ),
 ]
--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path("user/", views.UserView.as_view()),
-    path("moderators/", views.ModeratorsView.as_view()),
-    path("moderators/<int:user_id>/", views.ModeratorView.as_view()),
-    path("register/", views.RegisterUserView.as_view()),
+    path("user/", views.UserView.as_view(), name="user"),
+    path("moderators/", views.ModeratorsView.as_view(), name="moderators"),
+    path("moderators/<int:user_id>/", views.ModeratorView.as_view(), name="moderator"),
+    path("register/", views.RegisterUserView.as_view(), name="register"),
     path("token/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
